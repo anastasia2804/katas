97bfe7570f3e43efe84bd6e6ebbba8f20b8e1512
Anastasia Orlova <anastasia2804@gmail.com>
2022-11-19 09:24:43 -0500

    maxSubarraySum O(n^2) runtime

---
+++ JS-practice/MaxSubarraySum-O(n^2)/README.md
@@ -0,0 +1,1 @@
+Write a function maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array.
---
+++ JS-practice/MaxSubarraySum-O(n^2)/index.js
@@ -0,0 +1,19 @@
+function maxSubarraySum (myArr, num) {
+    if (myArr.length < num) return null
+
+    //can start with 0 assuming there are only positive integers
+    var max=-Infinity; 
+    for (let i = 0; i < myArr.length - num + 1; i++) {
+        var temp = 0;
+        for (let j=0; j< num; j++) {
+            temp += myArr[i+j]
+        }
+        if (temp > max) {
+            max=temp;
+        }
+        console.log(temp, max)
+    }
+    return max
+}
+
+console.log(maxSubarraySum([1,4,5,6,7,8,5,6,7], 3))
